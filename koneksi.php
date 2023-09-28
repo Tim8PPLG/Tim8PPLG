e57221150d19fdfc44de69670ad693deeceb2de2
Tim8PPLG <144774112+Tim8PPLG@users.noreply.github.com>
2023-09-28 22:09:46 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,1 +1,9 @@
+<?php 
+$koneksi = mysqli_connect("localhost","root","","multi_user");
 
+// check connection
+if (mysqli_connect_errno()){
+   echo "koneksi database gagal : " . mysqli_connect_errno();
+}
+
+ ?>
